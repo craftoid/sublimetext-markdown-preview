9de551a484b0375968ebf46825c763c78aa64dfa
Martin Schneider <craftoid@k2g2.org>
2013-02-26 20:32:40 +0100

    Make sure extensions are loaded automatically
    Sublime only loads python packages from the top level of a Plugin
    directory by default.
    
    Dynamically importing packages after the plugin has loaded
    is not possible. (See also
    http://www.sublimetext.com/forum/viewtopic.php?f=6&t=6777 )
    
    The solution is to add a package at the top level that automatically
    loads packages from the extension directory of this plugin.

---
+++ markdown_extensions.py
@@ -0,0 +1,21 @@
+"""
+Preload all python-markdown extensions
+"""
+
+# By default sublime only imports python packages from the top level of the plugin directory.
+# Trying to import packages from subdirectories dynamically at a later time is NOT possible.
+
+# This package automatically imports all packages from the extension directory
+# so they are available when we need them.
+
+import sublime, os, pkgutil
+
+packages_path = sublime.packages_path()
+package_name ="Markdown Preview"
+extension_module = "markdown.extensions"
+
+
+for  _, package, _ in pkgutil.walk_packages("."):
+	if package.startswith(extension_module):
+		print "Reloading plugin extension %s" % os.path.join(packages_path, plugin_name, *package.split(".")) + ".py"
+		__import__(package)
